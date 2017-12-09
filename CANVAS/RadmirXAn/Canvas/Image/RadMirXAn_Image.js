e2f8c35532d3f9ca761cbfa71d181e5c10eea2e9
RadMirXAn <rrradmirr@mail.ru>
2017-12-10 01:36:49 +0300

    Добавлена Хит область для картинок. Проверка на прозрачность.

--- CANVAS/RadmirXAn/Canvas/Image/RadMirXAn_Image.js
+++ CANVAS/RadmirXAn/Canvas/Image/RadMirXAn_Image.js
@@ -11,9 +11,18 @@ function  RadMirXAn_Image(image, layer) {
 		let hit = false;
 		let _mX =  x - this.x;
 		let _mY =  y - this.y;
-		if((0<_mX && _mX<image.width) &&  (0<_mY && _mY<image.height)){
+
+		context2D_hit.clearRect(0, 0, canvas_hit.width, canvas_hit.height);
+		context2D_hit.drawImage(image, 0, 0);
+		let pixel = context2D_hit.getImageData(_mX, _mY, 1, 1);
+		let data = pixel.data;
+		//let rgba = 'rgba(' + data[0] + ', ' + data[1] + ', ' + data[2] + ', ' + (data[3] / 255) + ')';
+		if((data[3] / 255)!=0){
 			hit = true;
+		}else{
+			hit = false;
 		}
+			
 		return hit;
 	}
 
--- CANVAS/RadmirXAn/Canvas/RadMirXAn_Canvas.js
+++ CANVAS/RadmirXAn/Canvas/RadMirXAn_Canvas.js
@@ -1,4 +1,6 @@
 //Создаем холст для рисования 1.
+var canvas_hit = document.getElementById("game_hit");
+var context2D_hit = canvas_hit.getContext("2d");
 var canvas = document.getElementById("game");
 var context2D = canvas.getContext("2d");
 //Создаем холст для рисования 0.
